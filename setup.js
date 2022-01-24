ff7a89b51035c766ef5f3b8d57f3e4cac99f4a2d
Naphat Amundsen <naphat@live.no>
2022-01-24 20:23:06 +0100

    more on setup

--- setup.js
+++ setup.js
@@ -5,9 +5,16 @@ import path from 'path'
 const BOUNTY_DIR = path.join(os.homedir(), '.bounty')
 const BOUNTY_CONFIG = path.join(BOUNTY_DIR, 'config.json')
 const BOUNTY_CONFIG_BASE = {
-  'serviceName': '',
-  'id': '',
-  'authorization': ''
+  'serviceName': null,
+  'id': null,
+  'authorization': null
+}
+const BOUNTY_DATA = path.join(BOUNTY_DIR, 'data.json')
+const BOUNTY_DATA_BASE = {
+  'initReferenceDate': null,
+  'initReferenceBalance': null,
+  'currReferenceDate': null,
+  'currReferenceBalance': null
 }
 
 /**
@@ -22,6 +29,9 @@ async function setupFilesInHome(dir = BOUNTY_DIR, config = BOUNTY_CONFIG, config
 
   if (!fs.existsSync(BOUNTY_CONFIG))
     fs.writeFileSync(BOUNTY_CONFIG, JSON.stringify(BOUNTY_CONFIG_BASE, null, 4))
+
+  if (!fs.existsSync(BOUNTY_DATA))
+    fs.writeFileSync(BOUNTY_DATA, JSON.stringify(BOUNTY_DATA_BASE, null, 4))
 }
 
 async function run() {
